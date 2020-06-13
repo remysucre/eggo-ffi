2570bfd2cf792440fd20508a29e437a22fc0e620
Remy <remywang@uw.edu>
2020-06-13 23:32:56 +0000

    Add convolution

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,33 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+use rand::prelude::*;
+
+unsafe fn new_w(graph: &mut root::taso::Graph, mut v: Vec<i32>, rng: &mut impl Rng) -> *mut root::taso::Tensor {
+      let vol = v.iter().product();
+      let data: Vec<f32> = (0..vol).map(|_| rng.gen()).collect();
+      graph.new_weight(v.len() as i32, v.as_ptr(), data.as_ptr())
+}
+
+unsafe fn new_i(graph: &mut root::taso::Graph, v: Vec<i32>) -> *mut root::taso::Tensor {
+      graph.new_input(v.len() as i32, v.as_ptr())
+}
+
 fn main() {
-    println!("Hello, world!");
     unsafe {
-      let mut dims = Vec::with_capacity(16);
-      dims.push(1);
-      dims.push(1);
-      dims.push(1);
-      dims.push(1);
+      let mut rng = rand::thread_rng();
+
       let mut graph = root::taso::Graph::new();
       root::taso::Graph_Graph(&mut graph);
-      // Segfaults here without the preceding line
-      let input = graph.new_input(dims.len() as i32, dims.as_ptr());
 
-      let mut w = Vec::with_capacity(16);
-      w.push(1);
-      w.push(1);
-      w.push(1);
-      w.push(1);
-      let weight = graph.new_input(w.len() as i32, w.as_ptr());
-      graph.matmul(input, weight, root::taso::ActiMode_AC_MODE_NONE);
+      let input = new_i(&mut graph, vec![1, 128, 56, 56]);
+      let w1 = new_w(&mut graph, vec![128, 128, 3, 3], &mut rng);
+      let w2 = new_w(&mut graph, vec![128, 128, 1, 1], &mut rng);
+      let w3 = new_w(&mut graph, vec![128, 128, 3, 3], &mut rng);
+
+      let left = graph.conv2d1(input, w1, 1, 1, root::taso::PaddingMode_PD_MODE_SAME, root::taso::ActiMode_AC_MODE_RELU);
+
+      // graph.matmul(input, w1, root::taso::ActiMode_AC_MODE_NONE);
       graph.optimize(1.0, 100, true);
       println!("{}", graph.total_cost());
     }
