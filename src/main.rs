240c3a5bc2407b412de6aea4ba1408b40fa5db18
Remy <remywang@uw.edu>
2020-06-14 01:08:08 +0000

    readme example

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,15 @@
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 use rand::prelude::*;
+use root::taso::*;
 
-unsafe fn new_w(graph: &mut root::taso::Graph, mut v: Vec<i32>, rng: &mut impl Rng) -> *mut root::taso::Tensor {
+unsafe fn new_w(graph: &mut Graph, mut v: Vec<i32>, rng: &mut impl Rng) -> *mut Tensor {
       let vol = v.iter().product();
       let data: Vec<f32> = (0..vol).map(|_| rng.gen()).collect();
       graph.new_weight(v.len() as i32, v.as_ptr(), data.as_ptr())
 }
 
-unsafe fn new_i(graph: &mut root::taso::Graph, v: Vec<i32>) -> *mut root::taso::Tensor {
+unsafe fn new_i(graph: &mut Graph, v: Vec<i32>) -> *mut Tensor {
       graph.new_input(v.len() as i32, v.as_ptr())
 }
 
@@ -20,18 +21,21 @@ fn main() {
     unsafe {
       let mut rng = rand::thread_rng();
 
-      let mut graph = root::taso::Graph::new();
-      root::taso::Graph_Graph(&mut graph);
+      let mut graph = Graph::new();
+      Graph_Graph(&mut graph);
 
       let input = new_i(&mut graph, vec![1, 128, 56, 56]);
       let w1 = new_w(&mut graph, vec![128, 128, 3, 3], &mut rng);
       let w2 = new_w(&mut graph, vec![128, 128, 1, 1], &mut rng);
       let w3 = new_w(&mut graph, vec![128, 128, 3, 3], &mut rng);
 
-      let left = graph.conv2d1(input, w1, 1, 1, root::taso::PaddingMode_PD_MODE_SAME, root::taso::ActiMode_AC_MODE_RELU);
+      let left = graph.conv2d1(input, w1, 1, 1, PaddingMode_PD_MODE_SAME, ActiMode_AC_MODE_RELU);
+      let mid = graph.conv2d1(left, w3, 1, 1, PaddingMode_PD_MODE_SAME, ActiMode_AC_MODE_NONE);
+      let right = graph.conv2d1(input, w2, 1, 1, PaddingMode_PD_MODE_SAME, ActiMode_AC_MODE_RELU);
+      let output = graph.element(OpType_OP_EW_ADD, mid, right);
+      graph.relu(output, false);
 
-      // graph.matmul(input, w1, root::taso::ActiMode_AC_MODE_NONE);
-      graph.optimize(1.0, 100, true);
+      graph.run();
       println!("{}", graph.total_cost());
     }
 }
